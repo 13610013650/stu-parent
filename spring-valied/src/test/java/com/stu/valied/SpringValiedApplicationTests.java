879e2c7d8edcc713ee6cf096a02dea457a019868
zhangsheng <zhangsheng@qq.com>
2020-05-18 18:18:06 +0800

    新增参数校验的demo工程

---
+++ spring-valied/src/test/java/com/stu/valied/SpringValiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stu.valied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
