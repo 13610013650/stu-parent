6c3bc240c8ce9300cbd4138a558bd7535fda72ce
zhangsheng_global <zhangsheng_global@qq.com>
2020-05-20 01:22:34 +0800

    新增接口幂等性模块

---
+++ spring-api-idempotence/src/main/java/com/stu/moudle/SpringApiIdempotenceApplication.java
@@ -0,0 +1,13 @@
+package com.stu.moudle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiIdempotenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiIdempotenceApplication.class, args);
+	}
+
+}
