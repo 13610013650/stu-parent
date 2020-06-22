a91e393b1a697f954f3ddceb62b6188486583831
zhangsheng <zhangsheng@qq.com>
2020-06-22 22:43:46 +0800

    提交线程应用.

---
+++ thread-demo/src/main/java/com/stu/thread/ThreadDeamoApplication.java
@@ -0,0 +1,13 @@
+package com.stu.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadDeamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadDeamoApplication.class, args);
+    }
+
+}
